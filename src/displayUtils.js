d428e27c8fea2c478b92688fb09a489dca38adac
beacon <75365338+vinhben12@users.noreply.github.com>
2024-07-27 18:06:45 +0700

    displayUtils.js

--- src/displayUtils.js
+++ src/displayUtils.js
@@ -4,8 +4,6 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=           Sonic Odyssey BOT          ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
